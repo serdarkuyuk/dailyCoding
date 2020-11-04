53e30c59926d5e1d64db87f1d770a0d249c56910
Serdar Kuyuk <serdarkuyuk@gmail.com>
2020-11-04 12:46:48 -0500

    commit

---
+++ 1431-kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,15 @@
+# candies = [2,3,5,1,3]
+# extraCandies = 3
+candies = [4,2,1,1,2]
+extraCandies = 1
+mylist = []
+candies2max = max(candies) - extraCandies
+
+#soution 1 with append (faster)
+for candie in candies:
+    mylist.append(candie >= candies2max )
+
+#solution 2 with list comprehension (slower)
+mylist = [candie >= candies2max for candie in candies]
+
+print(mylist)
