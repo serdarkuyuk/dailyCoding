9e850603ca9549e2cadf85a718c06a74c955f09b
Serdar Kuyuk <serdarkuyuk@gmail.com>
2020-11-04 15:39:33 -0500

    first

---
+++ 1512-number-of-good-pairs.py
@@ -0,0 +1,26 @@
+
+# nums = [1,2,3,1,1,3]
+# nums = []
+nums = [1,1,1,1]
+myDict = {}
+for num in nums:
+    if num not in myDict:
+        myDict.setdefault(num, 1)
+    else:
+        myDict[num] += 1
+
+def findfibonachi(n):
+    curr = 0
+    for i in range(n):
+        curr += i
+    return curr
+
+def findeasier(n):
+    return n*(n-1)/2
+
+summation = 0
+for key in myDict.keys():
+    if myDict[key] > 1:
+        summation += findeasier(myDict[key])
+
+print(summation)
